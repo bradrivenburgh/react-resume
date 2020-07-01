d1c63d5510e45dce3a397cabb11ff99aa25d9943
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-07-01 16:13:43 -0400

    refactored React.createElement to JSX

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
 
 const appRoot = document.querySelector('#application-root');
-const helloWorldH1 = React.createElement('h1', null, 'Hello, world!')
+const helloWorldH1 = <h1>Hello, world</h1>;
 ReactDOM.render(helloWorldH1, appRoot);
